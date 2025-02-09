801893d092f6d4ebc216ba583f663216de762086
Your Name <you@example.com>
2025-02-09 15:04:27 +0100

    correct letter generate method

--- src/main/java/pl/edu/agh/hangman/Generator.java
+++ src/main/java/pl/edu/agh/hangman/Generator.java
@@ -2,7 +2,6 @@ package pl.edu.agh.hangman;
 
 public class Generator {
 
-
     public void underscoreGenerate(String s) {
         if (!s.isEmpty()) {
             int stringLength = s.length();
@@ -13,13 +12,14 @@ public class Generator {
     }
 
     public String replaceUnserscoreWithLetter(String s, Character character) {
-        String replacedString = s.replaceAll("A-Za-z", "_");
+        String replacedString = s.replaceAll("[A-Za-z]", "_");
         s = replacedString.replace("*", character.toString());
         return s;
     }
 
     public String letterGenerate(String s, Character character) {
-        if (!s.isEmpty() && s.contains(String.valueOf(character))) {
+        String s1 = String.valueOf(character);
+        if (!s.isEmpty() && s.contains(s1)) {
             for (int i = 0; i < s.length(); i++) {
                 if (s.charAt(i) == character) {
                     s = s.replace(String.valueOf(character), "*");
